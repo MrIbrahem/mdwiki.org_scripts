3ffbfac6d90f9e3f0e31f9dd07a9ea39a76a5772
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-06-04 04:35:35 +0300

    .

--- flask_app/main_app/new_jobs/workers_list.py
+++ flask_app/main_app/new_jobs/workers_list.py
@@ -15,10 +15,12 @@ from .workers.import_history.worker import import_history_worker_entry
 class JobData:
     job_type: str
     job_name: str
-    job_callable: Callable
-
-    job_list_template: str
     job_details_template: Optional[str] = "jobs_templates/_help_templates/shared_details.html"
+    job_list_template: str
+
+    job_callable: Callable
+    job_args: list | None = None
+    start_confirm_message: str | None = None
 
 
 jobs_data = {
@@ -28,12 +30,16 @@ jobs_data = {
         job_details_template="one_page_templates/add_r_column/details.html",
         job_list_template="one_page_templates/add_r_column/list.html",
         job_callable=add_r_column_worker_entry,
+        job_args=[],
+        start_confirm_message="",
     ),
     "add_unlinkedwikibase": JobData(
         job_type="add_unlinkedwikibase",
         job_name="Add unlinkedwikibase tag",
         job_list_template="jobs_templates/add_unlinkedwikibase/list.html",
         job_callable=add_unlinkedwikibase_worker_entry,
+        job_args=[],
+        start_confirm_message="Start adding unlinkedwikibase tags to all pages?",
     ),
     "create_redirects": JobData(
         job_type="create_redirects",
@@ -41,6 +47,8 @@ jobs_data = {
         job_details_template="jobs_templates/create_redirects/details.html",
         job_list_template="jobs_templates/create_redirects/list.html",
         job_callable=create_redirects_worker_entry,
+        job_args=[],
+        start_confirm_message="",
     ),
     "duplicate_redirect": JobData(
         job_type="duplicate_redirect",
@@ -48,24 +56,32 @@ jobs_data = {
         job_details_template="jobs_templates/duplicate_redirect/details.html",
         job_list_template="jobs_templates/duplicate_redirect/list.html",
         job_callable=duplicate_redirect_worker_entry,
+        job_args=[],
+        start_confirm_message="",
     ),
     "find_and_replace": JobData(
         job_type="find_and_replace",
         job_name="Find and Replace",
         job_list_template="jobs_templates/find_and_replace/list.html",
         job_callable=find_and_replace_worker_entry,
+        job_args=[],
+        start_confirm_message="",
     ),
     "fixred_all": JobData(
         job_type="fixred_all",
         job_name="Fix Redirects in All Pages",
         job_list_template="jobs_templates/fixred_all/list.html",
         job_callable=fixred_all_worker_entry,
+        job_args=[],
+        start_confirm_message="",
     ),
     "fixref": JobData(
         job_type="fixref",
         job_name="Normalize References",
         job_list_template="jobs_templates/fixref/list.html",
         job_callable=fixref_worker_entry,
+        job_args=[],
+        start_confirm_message="",
     ),
     "import_history": JobData(
         job_type="import_history",
@@ -73,6 +89,8 @@ jobs_data = {
         job_details_template="jobs_templates/import_history/details.html",
         job_list_template="jobs_templates/import_history/list.html",
         job_callable=import_history_worker_entry,
+        job_args=[],
+        start_confirm_message="",
     ),
 }
 
